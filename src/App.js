2d1ec29fe8fe14edc46f931e03b83b6e9eef718f
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-10-14 22:24:00 +0330

    Add pie chart

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import Faq from "./scenes/faq";
 import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -34,9 +35,9 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<Faq />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
         </div>

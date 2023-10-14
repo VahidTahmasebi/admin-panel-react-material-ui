42bab33d83438255512724c96e38d58ae59a1aed
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-10-14 22:42:24 +0330

    Add line chart

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Calendar from "./scenes/calendar";
 import Faq from "./scenes/faq";
 import Bar from "./scenes/bar";
 import Pie from "./scenes/pie";
+import Line from "./scenes/line";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -36,7 +37,7 @@ function App() {
               <Route path="/faq" element={<Faq />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              {/* <Route path="/line" element={<Line />} /> */}
+              <Route path="/line" element={<Line />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
